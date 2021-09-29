396a9ae671c9f81050702d293374309d2e1e7a28
Hadi Arbabi <h.arbabi@cobex.ir>
2021-09-29 10:38:47 +0330

    halper api dollor price

--- jsdom.js
+++ jsdom.js
@@ -1,18 +1,22 @@
 import { JSDOM } from 'jsdom';
 import nodefetch from 'node-fetch';
 
-(async () => {
+async function usdApi() {
   const html = await nodefetch("https://www.tgju.org/profile/price_dollar_rl").then(data => { return data.text(); });
   const dom = new JSDOM(html);
   const { document } = await dom.window;
   const td = Array.from(document.querySelectorAll(`td`)).find(p => { return p.innerHTML === 'بالاترین قیمت روز'; });
+  const tdPrice = td.innerHTML;
   const price = td.nextElementSibling.querySelector('span').innerHTML;
   //change to riyal
   const usd = +(price.replace(/,/, '.')) * 1000;
-  console.log('tgju : ', +usd.toFixed(3));
-})();
+  console.log(`${tdPrice} : `, +usd.toFixed(3));
+};
 
 
+function interval() {
+  usdApi();
 
+}
 
-
+setInterval(interval,3600000)
--- puppeteer.js
+++ puppeteer.js
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
+  const start = Date.now()
   const browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true, userDataDir: './my/path' });
   const page = await browser.newPage();
   page.goto('https://www.tgju.org/profile/price_dollar_rl');
@@ -8,8 +9,11 @@ import puppeteer from 'puppeteer';
   const usd = await page.evaluate(() => {
     return document.querySelector(".price").innerHTML;
   });
-  await console.log(usd);
-  await console.log(+(usd.replace(/,/, '.')) / 10);
+  await console.log(+(usd.replace(/,/, '.')) * 1000);
+  const stop = Date.now()
+  console.log(`Time Taken to execute = ${(stop - start)/1000} seconds`);
+
+  process.exit(1);
 
 })();
 
